ede0a315a329c4c4c129617572e2ba7fccd82ae9
Sean Xiang <xiangs18@bu.ed>
2020-01-11 05:49:14 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphsage",
+    version="1.0",
+    description="graphsage-simple implementation",
+    packages=find_packages(),
+)
